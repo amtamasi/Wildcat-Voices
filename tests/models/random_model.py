7a1496fa11d1f3de7bf869de939c519f1e119ba9
Anthony <anthonymtamasi@gmail.com>
2020-04-08 17:26:26 -0400

    Built unittest skeleton for the random model

---
+++ tests/models/random_model.py
@@ -0,0 +1,17 @@
+"""
+    This file defines the tests for the random prediction model.
+"""
+import unittest
+
+from src.models import random_model
+
+class RandomModelTester(unittest.TestCase):
+    def test_train(self):
+        pass
+    
+    def test_predict(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
