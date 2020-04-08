83cba39592154ac5ac5620b59c11cf4e63514c46
Anthony <anthonymtamasi@gmail.com>
2020-04-08 00:33:42 -0400

    Implemented the RandomModel without the specific classes. self.classes initialization should be changed in future

--- src/models/random_model.py
+++ src/models/random_model.py
@@ -1,3 +1,16 @@
 """
     This file defines an interface for a random prediction model, used for a baseline.
 """
+import random
+
+from base_model import BaseModel
+
+class RandomModel(BaseModel):
+    def __init__(self):
+        self.classes = []
+    
+    def train(self):
+        pass
+
+    def predict(self):
+        return random.choice(self.classes)
