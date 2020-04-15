e92a4c5fe27ecd39d1c18f99824d195d85f84f3f
Chad Nelson <chadnelson@Chads-MacBook-Pro.local>
2020-04-14 22:26:28 -0400

    Added the save_model, get_data, and preprocess_data functions. I also made save_model, predict, and train functions raise a NotImplementedError if its child class does not implement them.

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,13 +1,37 @@
 """
     This file defines an interface for a base prediction model. 
+    This is the parent class for all machine learning models
 """
 
+
 class BaseModel():
+
     def __init__(self):
         pass
-    
+
+    # This function is not implemented here and is a virtual
+    # function and every child class must implement this function
     def train(self):
+        raise NotImplementedError()
+
+    # This function is not implemented here and is a virtual
+    # function and every child class must implement this function
+    @abc.abstractmethod
+    def predict(self):
+        raise NotImplementedError()
+
+    # This function is not implemented here and is a virtual
+    # function and every child class must implement this function
+    @abc.abstractmethod
+    def save_model(self):
+        raise NotImplementedError()
+
+    # This function will get the data that is needed to train
+    # the model
+    def get_data(self):
         pass
 
-    def predict(self):
+    # This function will process the audio file to minimize noise
+    # and have some consistentcy with the audio files
+    def preprocess_data(self):
         pass
