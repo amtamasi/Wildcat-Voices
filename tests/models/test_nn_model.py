b0f738516ff2059b43b9940394ead7949e380294
Anthony <anthonymtamasi@gmail.com>
2020-04-18 23:49:05 -0400

    Added unit tests for neural net model

---
+++ tests/models/test_nn_model.py
@@ -0,0 +1,24 @@
+"""
+    This file defines the tests for the random prediction model.
+"""
+import unittest
+
+from src.models.neural_net import NeuralNetModel
+
+class NeuralNetModelTester(unittest.TestCase):
+    def test_classes_not_empty(self):
+        model = NeuralNetModel()
+        num_classes = model.num_classes
+        self.assertTrue(num_classes > 0)
+
+    def test_train_output_type(self):
+        model = NeuralNetModel()
+        output_type = model.train()
+        self.assertIsInstance(output_type, float)
+
+    def test_predict(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
