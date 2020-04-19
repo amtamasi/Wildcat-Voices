bce80d81332902c9651d2f0fef1a473809eab26a
Chad Nelson <chadnelson@Chads-MacBook-Pro.local>
2020-04-18 21:15:49 -0400

    Added the test cases for the svm class.

--- src/models/svm.py
+++ src/models/svm.py
@@ -3,7 +3,7 @@
     Vector Machine machine learning model.
 """
 
-import base_model
+from .base_model import BaseModel
 from sklearn import svm
 # This can be used to read in the audio file
 # possible that this gets used in the BaseModel class
@@ -14,8 +14,7 @@ class SVM(BaseModel):
         self.max_iter = -1
         self.kernel_type = 'rbf'
         self.reg_param = 1.0
-        self.model = None
-        self.save_model
+        self.model = svm.SVC(C=self.reg_param, kernel=self.kernel_type, max_iter=self.max_iter)
 
     # Trains the model on the data
     # This function implements a virtual function in the base model
@@ -31,9 +30,6 @@ class SVM(BaseModel):
         # an audio file 
         # Get the file from the database
 
-        # Feed in the preprocessed data from the file
-        # to get a prediction
-
         # Return the prediction
         pass
 
@@ -45,6 +41,8 @@ class SVM(BaseModel):
     def set_kernel(self, kernel):
         self.kernel_type = kernel
         
-    def set_max_iterations(self, interations):
-        self.max_iter = interations
+    def set_max_iterations(self, iterations):
+        self.max_iter = iterations
 
+    def set_reg_param(self, reg_param):
+        self.reg_param = reg_param
--- tests/models/test_svm_model.py
+++ tests/models/test_svm_model.py
@@ -3,24 +3,49 @@
 """
 
 import unittest
-
+from sklearn import svm
 from src.models.svm import SVM
 
 class SVMModelTester(unittest.TestCase):
+    
+    def setUp(self):
+        self.test_model = SVM()
+
     def test_train(self):
+        # Check that self.get_data was called
+        # Check that self.model.fit was called with given parameters
         pass
 
     def test_predict(self):
+        # Check that reading the file was called
+        # Check that self.model.predict was called with given parameters
         pass
 
     def test_save_model(self):
-        pass
+        kernel = 'sigmoid'
+        max_iter = 100
+        reg_param = 2.0
+        svm_model = svm.SVC(C=reg_param, kernel=kernel, max_iter=max_iter)
+        self.test_model.kernel_type = kernel
+        self.test_model.max_iter = max_iter
+        self.test_model.reg_param = reg_param
+        self.test_model.save_model()
+        self.assertEqual(self.test_model.model.get_params(), svm_model.get_params())
 
     def test_set_kernel(self):
-        pass
+        kernel = 'sigmoid'
+        self.test_model.set_kernel(kernel)
+        self.assertEqual(self.test_model.kernel_type, kernel)
 
     def test_set_max_iterations(self):
-        pass
+        max_iter = 100
+        self.test_model.set_max_iterations(max_iter)
+        self.assertEqual(self.test_model.max_iter, max_iter)
 
-if __name__ = "__main":
+    def test_set_reg_param(self):
+        reg_param = 2.0
+        self.test_model.set_reg_param(reg_param)
+        self.assertEqual(self.test_model.reg_param, reg_param)
+
+if __name__ == "__main":
     unittest.main()
