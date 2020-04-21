d7600deabccb165f1b349e94002e235a4f7f9101
Chad Nelson <chadnelson@Chads-MacBook-Pro.local>
2020-04-21 00:38:33 -0400

    Changes

--- app.py
+++ app.py
@@ -13,12 +13,19 @@ rand_model = RandomModel()
 
 # BUG: sklearn module error
 svm_model = SVM()
+<<<<<<< HEAD
+NN_model = NeuralNetModel()
+rand_model.load_model()
+NN_model.load_model()
+svm_model.load_model()
+=======
 svm_model.load_model()
 
 # BUG: Threading issues between flask and tensorflow
 #nn_model = NeuralNetModel()
 #nn_model.load_model("model.json", "weights.h5")
 #graph = nn_model.graph
+>>>>>>> 891ca220821319da731d07081ce3e9d9ad5ee5f9
 
 @app.route('/')
 def home():
@@ -26,7 +33,18 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    file_name = request.form['audio-file']
     # NOTE: request.form.values() needs to be preprocessed audio sample
+<<<<<<< HEAD
+    rand_prediction = rand_model.predict()
+    svm_features = svm_model.process_data(file_name)
+    svm_prediction = svm_model.predict(svm_features)
+    #NN_features = NN_predictions = NN_model.process_data(file_name)
+    #NN_prediction = NN_model.predict(NN_features)
+
+    # return prediction
+    return render_template("index.html", random_text="Area of Origin: {}".format(rand_prediction), svm_test="Area of Origin: {}".format(svm_prediction))
+=======
     #rand_prediction = RandomModel.predict()
     svm_prediction = svm_model.predict(request.form["audio-file"])
 
@@ -40,17 +58,22 @@ def predict():
 
     # return prediction
     return render_template("index.html", prediction_text="Area of Origin: {}".format(svm_prediction))
+>>>>>>> 891ca220821319da731d07081ce3e9d9ad5ee5f9
 
     # NOTE: testing
     #return render_template("index.html", prediction_text="Area of Origin: {}".format("Kentucky"))
-
+    
 """
+
 @app.route('/results',methods=['POST'])
 def results():
+
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
+
     output = prediction[0]
     return jsonify(output)
+
 """
 
 if __name__ == "__main__":
